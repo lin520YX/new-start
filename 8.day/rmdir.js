77b32635cb61d30da012c5a5c5e9036cd5d0f661
lin13177896353@163.com <lin13177896353@163.com>
2018-11-07 07:51:16 +0800

    8change

--- 8.day/rmdir.js
+++ 8.day/rmdir.js
@@ -8,18 +8,49 @@ let path = require('path')
 
 // }
 
-function removeDepSync(p){
-    let statObj = fs.statSync(p);
-    if(statObj.isDirectory()){
-        // 删除子文件
-       let dirs = fs.readdirSync(p);
-       dirs.map(d=>path.join(p,d));
-       dirs.forEach(element => {
-           removeDepSync(element);
-       });
-       fs.rmdirSync(p);
-    }else{
-        fs.unlinkSync(p);
-    }
+// function removeDepSync(p) {
+//     let statObj = fs.statSync(p);
+//     if (statObj.isDirectory()) {
+//         // 删除子文件
+//         let dirs = fs.readdirSync(p);
+//         dirs = dirs.map(d => path.join(p, d));
+//         dirs.forEach(element => {
+//             removeDepSync(element);
+//         });
+//         fs.rmdirSync(p);
+//     } else {
+//         fs.unlinkSync(p);
+//     }
+// }
+// removeDepSync('m')
+
+function removeDep(p, cb) {
+    fs.stat(p, function (err, statObj) {
+        //    判断是文件夹还是文件
+        if (statObj.isDirectory()) {
+            /**
+             * 第一次 next 执行走的是removeDep 第二次执行的是第二个 第三次 走的是第一次removeDep
+             */ 
+            fs.readdir(p, function (err, dirs) {
+                dirs = dirs.map(d => path.join(p, d));
+                function next(index) {
+                    if (index === dirs.length) return fs.rmdir(p, cb);
+                    let currentPath = dirs[index];
+                    // 删除第一个儿子
+                    // 删除完成之后再去删除 下一项目
+                    removeDep(currentPath, () => next(index + 1));
+                }
+                next(0);
+            })
+        } else {
+            fs.unlink(p, cb);
+        }
+    });
 }
-removeDepSync('m')
+
+// fs.stat()，它返回一个Stat对象，能告诉我们文件或目录的详细信息：
+// 1.异步版：fs.stat(path,callback):
+//     path是一个表示路径的字符串,callback接收两个参数(err,stats),其中stats就是fs.stats的一个实例；
+
+// 2.同步版：fs.statSync(path)
+//    只接收一个path变量，fs.statSync(path)其实是一个fs.stats的一个实例；
