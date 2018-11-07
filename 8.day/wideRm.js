6b5420753608423bc321430e8e676366338b9e76
lin13177896353@163.com <lin13177896353@163.com>
2018-11-07 18:06:29 +0800

    wideRm

--- 8.day/wideRm.js
+++ 8.day/wideRm.js
@@ -11,15 +11,18 @@ function wide(p, cb) {
                 fs.readdir(current, (err, dirs) => {
                     dirs = dirs.map(dir => path.join(current, dir));
                     arr = [...arr, ...dirs];
-
+                    // 下
                     next(index + 1);
                 });
             } else {
+                // 下
                 next(index + 1);
             }
         });
     }
     next(0);
+    
+    // 递归删除 5,3,3,2,1
     function remove() {
         function next(index) {
             if (index < 0) return cb();
@@ -39,25 +42,26 @@ wide('m', () => {
     console.log('delete')
 })
 
-let {promisify} = require('util')
+let { promisify } = require('util')
 let stat = promisify(fs.stat);
 let readdir = promisify(fs.readdir);
 let unlink = promisify(fs.unlink);
 let rmdir = promisify(fs.rmdir);
-async function wideRm(p){
-    let index =0;
+async function wideRm(p) {
+    let index = 0;
     let arr = [p];
-    while(index !== arr.length){
+    while (index !== arr.length) {
         let current = arr[index];
-       let statObj =await stat(current);
-       if(statObj.isDirectory()){
+        let statObj = await stat(current);
+        if (statObj.isDirectory()) {
             let dirs = await readdir(current);
-            dirs= dirs.map(d=>path.join(current,d))
-            arr=[...arr,...dirs];
-       }
-        index ++;
+            dirs = dirs.map(d => path.join(current, d))
+            arr = [...arr, ...dirs];
+        }
+        index++;
     }
-    let current; 
+    // 记录地址
+    let current='';
     while ((current = arr.pop())) {
         let statObj = await stat(current);
         if (statObj.isDirectory()) {
@@ -67,6 +71,6 @@ async function wideRm(p){
         }
     }
 }
-wideRm('m').then(()=>{
+wideRm('m').then(() => {
     console.log('deletr')
 })
