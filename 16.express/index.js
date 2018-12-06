8f3cce92193b47ced9359e8083c93728d8d658f5
lin13177896353@163.com <lin13177896353@163.com>
2018-12-07 00:12:27 +0800

    express-all

--- 16.express/index.js
+++ 16.express/index.js
@@ -1,28 +1,35 @@
 let http = require('http');
-let methods= require('methods');
+let methods = require('methods');
 let url = require('url');
-function Application(){
-    function app(req,res){
-        let {pathname} = url.parse(req.url,true)
+function Application() {
+    function app(req, res) {
+        let { pathname } = url.parse(req.url, true)
         let method = req.method.toLowerCase();
-        for(let i =0 ;i<app.routes.length;i++){
+        for (let i = 0; i < app.routes.length; i++) {
             let layer = app.routes[i];
-            let {method:m,path,callback} = layer;
-            if(method == m&&path==pathname){
-                return callback(req,res);
+            let { method: m, path, callback } = layer;
+            if (((method == m) || (m == 'all')) && ((path == pathname) || (path == '*'))) {
+                return callback(req, res);
             }
         }
-        res.end(`cannot ${method} ${pathname}`)
+        res.end(`cannot ${method} ${pathnamenm}`)
     }
-    methods.forEach(method=>{
-        app[method]=function(path,callback){
-            let layer ={
-                method,path,callback
-             }
-             app.routes.push(layer);
+    [...methods, 'all'].forEach(method => {
+        app[method] = function (path, callback) {
+            let layer = {
+                method, path, callback
+            }
+            if (path.include(':')) {
+                layer.path = new RegExp(path.replace(/:([^\/])/g,function(){
+                    layer.params.push(arguments[1]);
+                    return '([^\/])'
+                }));
+            }
+            app.routes.push(layer);
         }
+
     })
-    app.listen(function(...args){
+    app.listen(function (...args) {
         let server = http.createServer(app);
         server.listen(...args);
     })
