2b88f92e4db452de19c5df95d57f80424baa425a
lin13177896353@163.com <lin13177896353@163.com>
2018-12-30 17:55:14 +0800

    reduxMiddleWare

--- 19.react-redux/src/index.js
+++ 19.react-redux/src/index.js
@@ -4,6 +4,7 @@ import store from './store/index';
 import { Provider } from './react-redux';
 import Counter from './components/Counter';
 console.log(store)
+// 多个中间件进行组合
 ReactDom.render(
     <Provider store={store}>
         <Counter></Counter>
--- 19.react-redux/src/store/index.js
+++ 19.react-redux/src/store/index.js
@@ -1,4 +1,20 @@
 import { createStore } from 'redux';
 import reducer from './reducer/index';
-let store = createStore(reducer);
+// import reduxLogger from 'redux-logger';
+let reduxLogger = (store) => (dispatch) => (action) => {
+    console.log(store.getState())
+    dispatch(action)
+    console.log(store.getState())
+}
+let applyMiddleware = (middleWare) => (createStore) => (reducer) => {
+    let store = createStore(reducer)
+    let middle = middleWare(store)
+    let dispatch = middle(store.dispatch)
+    return {
+        ...store,
+        dispatch
+    }
+}
+let store = applyMiddleware(reduxLogger)(createStore)(reducer)
+
 export default store;
