44713434adefc63674ad3e8f40e2edc8f789d199
lin13177896353@163.com <lin13177896353@163.com>
2018-12-31 23:59:13 +0800

    .

--- 19.react-redux/src/index.js
+++ 19.react-redux/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from './react-redux';
 import Counter from './components/Counter';
 console.log(store)
 // 多个中间件进行组合
+
 ReactDom.render(
     <Provider store={store}>
         <Counter></Counter>
