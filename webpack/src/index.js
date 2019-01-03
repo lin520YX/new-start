4297ab8037a5799568deeea1494f430bf451b3a2
lin13177896353@163.com <lin13177896353@163.com>
2019-01-04 00:39:35 +0800

    webpackconfig

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,2 +1,7 @@
+import './index.css'
 let a = require('./a');
-console.log(a)
+console.log(a+'1');
+let fn =()=>{
+    console.log('hello')
+}
+fn()
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,18 +1,50 @@
 // webpack 配置文件 commonjs规范基于node
 let path = require('path');
 let HtmlWebpackPlugin = require('html-webpack-plugin')
-module.exports={
-    mode:'development',
-    entry:"./src/index.js", // 入口文件
-    output:{
-        path: path.resolve(__dirname,'dist'), // 绝对路径
-        filename:'bundle.js'
+module.exports = {
+    mode: 'development',
+    entry: "./src/index.js", // 入口文件
+    output: {
+        path: path.resolve(__dirname, 'dist'), // 绝对路径
+        filename: 'bundle.js'
+    },
+    // css-loader 解析文件中的 @import
+    module: {
+        rules: [{
+            test: /\.css/,
+            use: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.js/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env'
+                    ],
+                    plugins: [
+                        '@babel/plugin-proposal-class-properties',
+                        '@babel/plugin-proposal-decorators'
+                    ]
+                }
+            },
+            excludes: /node_modules/
+        }
+        ]
+    },
+    devServer: {
+        contentBase: './dist',
+        port: 3000,
+        progress: true,
+        compress: true
     },
     //webpack 配置 loader 转化器 加载器皿
     // plugin 插件
-    plugin:[
+    plugins: [
         new HtmlWebpackPlugin({
-            template:'./public/index.html',
+            filename: 'index.html',
+            template: './public/index.html',
+            hash: true
         })
     ]
 
