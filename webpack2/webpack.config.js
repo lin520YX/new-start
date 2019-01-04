a5ebeb87c3ce2c7abdb16b47292ddaaea66e4aa2
lin13177896353@163.com <lin13177896353@163.com>
2019-01-04 20:01:02 +0800

    eslint

--- webpack2/webpack.config.js
+++ webpack2/webpack.config.js
@@ -3,56 +3,54 @@ let HtmlWebpackPlugin = require('html-webpack-plugin');
 let MiniCssExtractPlugin = require('mini-css-extract-plugin');
 let UglifyWebpackPlugin = require('uglify-webpack-plugin');
 let OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-module.exports={
-    optimization:{
-        minimizer:[
+module.exports = {
+    optimization: {
+        minimizer: [
             new UglifyWebpackPlugin({
-                cache:true,
-                parallel:trye
+                cache: true,
+                parallel: trye
             }),
             new OptimizeCssAssetsPlugin({
             })
         ]
     },
-    mode:'development',
-    entry:{
-        main:'./src/index.js'
+    mode: 'development',
+    entry: {
+        main: './src/index.js'
     },
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:'[name].js'
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
+            // 代码校验eslint eslint-loader
+            // {
+            //     test:/\.js/,
+            //     use:'eslint-loader',
+            //     enforce:'pre' 
+            // },
             {
-                test:/\.css/,
-                use:[
+                test: /\.css/,
+                use: [
                     MiniCssExtractPlugin.loader
-                ,'css-loader','postcss-loader']
+                    , 'css-loader', 'postcss-loader']
             },
             {
-                test:/\.js/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env'
-                        ],
-                        plugins: [
-                        ]
-                    }
-                },
+                test: /\.js/,
+                excludes: /node_modules/,
+                use: [ 'babel-loader','eslint-loader'],
             }
         ]
     },
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            template:'./public/index.html',
-            filename:'index.html',
-            chunk:['main']
+            template: './public/index.html',
+            filename: 'index.html',
+            chunk: ['main']
         }),
         new MiniCssExtractPlugin({
-            filename:'main.css',
+            filename: 'main.css',
         })
     ]
 }
